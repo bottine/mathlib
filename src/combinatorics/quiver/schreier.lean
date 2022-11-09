bc007d73dae13ab946db30768df41764b0750225
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2022-11-09 11:21:29 +0100

    it's harder than I want it to be

--- src/combinatorics/quiver/schreier.lean
+++ src/combinatorics/quiver/schreier.lean
@@ -8,6 +8,7 @@ import data.list
 import algebra.group.defs
 import group_theory.group_action.basic
 import group_theory.group_action.group
+import group_theory.group_action.defs
 import group_theory.subgroup.basic
 import group_theory.coset
 import group_theory.quotient_group
@@ -40,8 +41,6 @@ inductive schreier_graph.arrow (V : Type*) {M S : Type*} [has_smul M V] (ι : S 
 
 def schreier_graph (V : Type*) {M S : Type*} [has_smul M V] (ι : S → M) := V
 
-
-
 instance (V : Type*) {M S : Type*} [h : has_smul M V] (ι : S → M) :
   has_smul M (schreier_graph V ι) := h
 
@@ -51,6 +50,10 @@ variables (V : Type*) {M S : Type*} [has_smul M V] (ι : S → M)
 
 def to_schreier_graph : V → sg V ι := id
 def of_schreier_graph : sg V ι → V := id
+
+@[simp]
+lemma schreier_graph_eq : sg V ι = V := rfl
+
 
 instance schreier_graph_quiver : quiver (sg V ι) := ⟨schreier_graph.arrow V ι⟩
 instance schreier_graph_coloring : quiver.coloring (sg V ι) S :=
@@ -75,22 +78,46 @@ section group_action
 
 variables {V : Type*} {G S : Type*} [group G] [mul_action G V] (ι : S → G)
 
-abbreviation schreier_coset_graph (H : subgroup G) := sg (G ⧸ H) ι
+abbreviation schreier_coset_graph (H : subgroup G) := schreier_graph (G ⧸ H) ι
+
+/-
+noncomputable def equiv_coset_graph_obj [mul_action.is_pretransitive G V] (x₀ : V) :
+  schreier_coset_graph ι (mul_action.stabilizer G x₀) → schreier_graph V ι :=
+(mul_action.equiv_quotient_stabilizer G x₀).symm.to_fun
+
+noncomputable def equiv_coset_graph_map [mul_action.is_pretransitive G V] (x₀ : V)
+  (X Y : schreier_coset_graph ι (mul_action.stabilizer G x₀)) (f : X ⟶ Y) :
+  (equiv_coset_graph_obj ι x₀ X ⟶ equiv_coset_graph_obj ι x₀ Y) :=
+begin
+  revert f X Y,
+  apply schreier_graph.arrow.rec,
+  rintro m x,
+  fapply quot.hrec_on x,
+  rintro g,
+  dsimp [equiv_coset_graph_obj, mul_action.equiv_quotient_stabilizer,
+         mul_action.of_quotient_stabilizer, quotient.lift_on', quotient.lift_on,quot.lift_on,
+         has_smul.smul, quotient.map', quot.map],
+  rw [mul_smul (ι m) g x₀], constructor,
+  rintro g h gh, simp,
+  have : g • x₀ = h • x₀, by
+  { symmetry,
+    rw [←inv_smul_eq_iff, ←mul_smul, ←mul_action.mem_stabilizer_iff,
+        ←quotient_group.left_rel_apply], exact gh, },
+
+
+end
 
 noncomputable def equiv_coset_graph [mul_action.is_pretransitive G V] (x₀ : V) :
   schreier_coset_graph ι (mul_action.stabilizer G x₀) ≃q schreier_graph V ι :=
-{ obj := (mul_action.equiv_quotient_stabilizer G x₀).symm.to_fun,
+{ obj := equiv_coset_graph_obj ι x₀,
   bij_obj := by { apply equiv.bijective, },
-  map := by
-  { rintros _ _ ⟨X,S⟩,
-    dsimp [mul_action.equiv_quotient_stabilizer, mul_action.of_quotient_stabilizer],
-    dsimp [quotient.lift_on', quotient.lift_on, quot.lift_on],
-    sorry, },
+  map := sorry,
   bij_map := sorry }
 
 lemma equiv_coset_graph.color [mul_action.is_pretransitive G V] (x₀ : V)
   {X Y : schreier_coset_graph ι (mul_action.stabilizer G x₀)} (f : X ⟶ Y) :
   color S ((equiv_coset_graph ι x₀).map f) = color S f := sorry
+-/
 
 end group_action
 
