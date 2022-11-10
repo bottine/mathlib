fc960cc1b7bf35496cfa025b2b38332711114858
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2022-11-10 17:58:44 +0100

    symmetrify cover

--- src/combinatorics/quiver/cover.lean
+++ src/combinatorics/quiver/cover.lean
@@ -44,37 +44,56 @@ begin
 end
 lemma comp' (hψ : ψ.is_covering) (hφψ : (φ ≫q ψ).is_covering ) : φ.is_covering :=
 begin
-  simp [prefunctor.is_covering],
-  split,
-  { rintro u,
-    rw ←@function.bijective.of_comp_iff' _ _ _ (ψ.star $ φ.obj u) (hψ.left $ φ.obj u) (φ.star u),
+  split;
+  rintro u,
+  { rw ←@function.bijective.of_comp_iff' _ _ _ (ψ.star $ φ.obj u) (hψ.left $ φ.obj u) (φ.star u),
     exact hφψ.left u},
-  { rintro u,
-    rw ←@function.bijective.of_comp_iff' _ _ _ (ψ.costar $ φ.obj u) (hψ.right $ φ.obj u) (φ.costar u),
+  { rw ←@function.bijective.of_comp_iff' _ _ _ (ψ.costar $ φ.obj u) (hψ.right $ φ.obj u) (φ.costar u),
     exact hφψ.right u},
 end
 lemma comp'' (hφ : φ.is_covering) (hφψ : (φ ≫q ψ).is_covering) (φsur : function.surjective φ.obj) :
   ψ.is_covering :=
 begin
-  simp [prefunctor.is_covering],
-  split,
-  { rintro v,
-    obtain ⟨u,rfl⟩ := φsur v,
-    rw ←@function.bijective.of_comp_iff _ _ _ (ψ.star $ φ.obj u) (φ.star u)  (hφ.left u),
+  split;
+  rintro v;
+  obtain ⟨u,rfl⟩ := φsur v,
+  { rw ←@function.bijective.of_comp_iff _ _ _ (ψ.star $ φ.obj u) (φ.star u)  (hφ.left u),
     exact hφψ.left u, },
-  { rintro v,
-    obtain ⟨u,rfl⟩ := φsur v,
-    rw ←@function.bijective.of_comp_iff _ _ _ (ψ.costar $ φ.obj u) (φ.costar u)  (hφ.right u),
+  { rw ←@function.bijective.of_comp_iff _ _ _ (ψ.costar $ φ.obj u) (φ.costar u)  (hφ.right u),
     exact hφψ.right u, },
 end
 
 def prefunctor.symmetrify : (symmetrify U) ⟶q (symmetrify V) :=
 { obj := φ.obj,
-  map := λ X Y f, sum.cases_on f (λ f, sum.inl (φ.map f)) ((λ g, sum.inr (φ.map g))) }
+  map := λ X Y, sum.map φ.map φ.map }
+
+def symmetrify_star (u : U) : star (symmetrify.of.obj u) ≃ star u ⊕ costar u :=
+begin
+  fsplit,
+  { rintro ⟨v,(f|g)⟩, exact sum.inl ⟨v,f⟩, exact sum.inr ⟨v,g⟩, },
+  { rintro (⟨v,f⟩|⟨v,g⟩), exact ⟨v,quiver.hom.to_pos f⟩, exact ⟨v,quiver.hom.to_neg g⟩, },
+  { rintro ⟨v,(f|g)⟩, simp, },
+  { rintro (⟨v,f⟩|⟨v,g⟩), simp, },
+end
+
+def symmetrify_costar (u : U) : costar (symmetrify.of.obj u) ≃ costar u ⊕ star u := sorry
+
+lemma prefunctor.symmetrify_star (u : U) : (φ.symmetrify.star u) =
+ (symmetrify_star (φ.obj u)).symm ∘ (sum.map (φ.star u) (φ.costar u)) ∘ (symmetrify_star u) := sorry
+
+lemma prefunctor.symmetrify_costar (u : U) : (φ.symmetrify.costar u) =
+ (symmetrify_costar (φ.obj u)).symm ∘ (sum.map (φ.costar u) (φ.star u)) ∘ (symmetrify_costar u) := sorry
+
 
 lemma is_covering.symmetrify (hφ : φ.is_covering) : φ.symmetrify.is_covering :=
 begin
-  sorry,
+  split;
+  rintro u,
+  rw φ.symmetrify_star u, simp, sorry,
+  rw φ.symmetrify_costar u, simp, sorry,
+  -- a sum of bijective functions is bijective
+
+
 end
 
 @[reducible] def path_from (u : U) := Σ v : U, path u v
