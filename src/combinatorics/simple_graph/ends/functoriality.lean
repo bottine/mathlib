a231cc6a4332af1ec778d45faff5b0e23ceeb993
ART0 <18333981+0Art0@users.noreply.github.com>
2022-12-16 11:08:07 +0530

    Add bare skeleton for functoriality

---
+++ src/combinatorics/simple_graph/ends/functoriality.lean
@@ -0,0 +1,19 @@
+import .defs
+
+/-!
+
+Sufficiently nice maps between graphs induce maps between connected components.
+In other words, there is a functor from the category of simple graphs with appropriate morphisms
+to the category of sets, taking each graph to its set of connected components.
+
+The required maps between graphs are cofinite (maps that pull back finite sets to finite sets) weak homomorphisms 
+(maps that send reachable pairs of points to reachable pairs of points).
+-/
+
+/- Weak homomorphisms -/
+
+/- Cofinite maps (TODO: Use the earlier code) -/
+
+/- Properties -/
+
+/- Functor -/
