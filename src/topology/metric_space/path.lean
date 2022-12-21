d6fbf4ecfc0801a15ebc195b2a84624de8cbe0b0
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2022-12-21 12:21:57 +0100

    wip

--- src/topology/metric_space/path.lean
+++ src/topology/metric_space/path.lean
@@ -5,67 +5,11 @@ import topology.metric_space.emetric_space
 import topology.metric_space.basic
 import data.list.destutter
 import topology.instances.ennreal
-
-noncomputable theory
-
-open_locale classical ennreal
+import .path_preliminaries
 
 open emetric nnreal set
 
-section preliminaries
-
-variables {α : Type*} (x : α)
-
-
-lemma subtype.map_id_comp_id (α : Type*) (r s t : α → Prop)
-  (rs : ∀ x, r x → s x) (st : ∀ x, s x → t x) :
-  (subtype.map id st) ∘ (subtype.map id rs) = (subtype.map id $ λ x, (st x) ∘ (rs x)) :=
-funext (λ _, rfl)
-
-lemma subtype.coe_comp_map_id (α : Type*) (r s : α → Prop)
-  (rs : ∀ x, r x → s x) :
-  coe ∘ (subtype.map id rs) = (coe : (subtype r) → α) :=
-funext (λ _, rfl)
-
-def list.take_while_subtype [preorder α] (l : list α) : list (subtype (≤x)) :=
-(l.take_while (≤x)).attach.map $ subtype.map id $ λ y, list.mem_take_while_imp
-
-lemma list.take_while_subtype_map_coe [preorder α] (l : list α) :
-  (l.take_while_subtype x).map coe = l.take_while (≤x) :=
-begin
-  dsimp [list.take_while_subtype],
-  simp only [list.map_map],
-  apply list.attach_map_coe,
-end
-
-lemma list.pairwise_le_drop_while_le_not_le  [preorder α] :
-  ∀ (l : list α) (h : l.pairwise (≤)) (y : α), y ∈ l.drop_while (≤x) → ¬y ≤ x
-| [] h y hy := by { simp only [list.drop_while, list.not_mem_nil] at hy, exact hy.elim }
-| (a::l) h y hy := by
-  { dsimp [list.drop_while] at hy,
-    simp only [list.pairwise_cons] at h,
-    split_ifs at hy with ax nax,
-    { exact list.pairwise_le_drop_while_le_not_le l h.right y hy, },
-    { cases hy,
-      { cases hy, exact ax},
-      { rintro yx, exact ax ((h.left y hy).trans yx), }, }, }
-
-def list.drop_while_subtype [preorder α] (l : list α) (h : l.pairwise (≤)) : list (subtype (λ y, ¬ y≤x)) :=
-(l.drop_while (≤x)).attach.map $ subtype.map id (λ y, l.pairwise_le_drop_while_le_not_le x h y)
-
-def list.drop_while_subtype_ge [linear_order α] (l : list α) (h : l.pairwise (≤)) :
-  list (subtype (λ y, x≤y)) :=
-(l.drop_while_subtype x h).map $ subtype.map id $ λ y h', @le_of_not_le α _ y x h'
-
-lemma list.drop_while_subtype_ge_map_coe [linear_order α] (l : list α)(h : l.pairwise (≤)) :
-  (l.drop_while_subtype_ge x h).map coe = l.drop_while (≤x) :=
-begin
-  dsimp [list.drop_while_subtype_ge, list.drop_while_subtype],
-  simp [list.map_map, subtype.map_id_comp_id, subtype.coe_comp_map_id],
-  finish,
-end
-
-end preliminaries
+noncomputable theory
 
 namespace function
 
@@ -145,7 +89,6 @@ lemma edist_mem_le_length_on_cons :
       apply add_le_add_right _ (length_on f (y :: l)),
       apply edist_triangle, } }
 
-
 lemma edist_le_length_on :
   ∀ {a b : β} {l : list β} (al : a ∈ l) (bl : b ∈ l), edist (f a) (f b) ≤ f.length_on l
 | a b [] ha hb := (list.not_mem_nil _ ha).elim
@@ -175,7 +118,6 @@ le_trans (self_le_add_right _ _) (f.length_on_append l l')
 lemma length_on_le_length_on_append_right
   (l l' : list β) : f.length_on l' ≤ f.length_on (l ++ l') :=
 le_trans (self_le_add_left _ _) (f.length_on_append l l')
-
 
 lemma length_on_reverse : ∀ (l : list β), f.length_on l.reverse = f.length_on l
 | [] := rfl
@@ -284,18 +226,9 @@ begin
   apply le_antisymm,
   apply f.path_length_comp_mono φ mφ,
 
-  -- Yael should know a lemma for this
   obtain ⟨ψ,φψ⟩ := sφ.has_right_inverse,
-  have mψ : monotone ψ := by
-  { rintro x y l,
-    have := le_total (ψ x) (ψ y),
-    cases this,
-    { exact this, },
-    { let := mφ (this),
-      rw [φψ x, φψ y] at this,
-      cases le_antisymm l this, refl, } },
 
-  convert (f ∘ φ).path_length_comp_mono ψ mψ,
+  convert (f ∘ φ).path_length_comp_mono ψ (right_inverse_monotone φ mφ ψ φψ),
   ext x,
   simp only [comp_app, φψ x],
 end
@@ -306,21 +239,14 @@ begin
   apply le_antisymm,
   apply f.path_length_comp_anti φ mφ,
 
-  -- Yael should know a lemma for this
   obtain ⟨ψ,φψ⟩ := sφ.has_right_inverse,
-  have mψ : antitone ψ := by
-  { rintro x y l,
-    have := le_total (ψ y) (ψ x),
-    cases this,
-    { exact this, },
-    { let := mφ (this),
-      rw [φψ x, φψ y] at this,
-      cases le_antisymm l this, refl, } },
 
-  convert (f ∘ φ).path_length_comp_anti ψ mψ,
+  convert (f ∘ φ).path_length_comp_anti ψ (right_inverse_antitone φ mφ ψ φψ),
   ext x,
   simp only [comp_app, φψ x],
 end
+
+set_option profiler true
 
 /-- Statement ~copied from Gouezel's bounded variation code.
 If `sl sr` are two subsets of `s`, `sl` to the left of `sr` the sum of the restricted lengths
@@ -328,15 +254,16 @@ is less or equal than the length.
 -/
 -- Probably deserves specializations for intervals
 lemma path_length_ge_parts (l r : set β) (lr : ∀ x ∈ l, ∀ y ∈ r, x ≤ y) :
-  (f ∘ (coe : subtype l → β)).path_length + (f ∘ (coe : subtype r → β)).path_length ≤ (f.path_length) :=
+  (f ∘ (coe : subtype l → β)).path_length
+  + (f ∘ (coe : subtype r → β)).path_length ≤ (f.path_length) :=
 begin
   apply ennreal.bsupr_add_bsupr_le sorted_list_nonempty sorted_list_nonempty,
   rintro left lefts right rights,
   simp_rw [←length_on_map f coe],
   apply (f.length_on_append _ _).trans,
-  dsimp [path_length],
+  dsimp only [path_length],
   refine @le_supr₂ _ _ _ _ (λ l H, f.length_on l) _ _,
-  rw list.pairwise_append,
+  rw [mem_set_of, list.pairwise_append],
   refine ⟨list.pairwise.map _ (λ a b h, h) lefts,
           list.pairwise.map _ (λ a b h, h) rights,
           λ x xleft y yright, _⟩,
@@ -349,33 +276,35 @@ end
 
 lemma path_length_glue_split (m : β) :
   f.path_length = (f ∘ (coe : {x // x ≤ m} → β)).path_length
-                + (f ∘ (coe : {x // x ≥ m} → β)).path_length :=
+                + (f ∘ (coe : {x // m ≤ x} → β)).path_length :=
 begin
-  dsimp [path_length],
+  dsimp only [path_length],
   apply le_antisymm,
   { rw supr₂_le_iff,
     rintro l ls,
-    let left := list.take_while (≤m) l,
-    let right := list.drop_while (≤m) l,
     rw ←list.take_while_append_drop (≤m) l,
     apply (length_on_le_append_singleton_append f _ m _).trans,
     rw length_on_append_singleton_append,
     refine add_le_add _ _,
-    { transitivity'
-        (f ∘ coe).length_on (l.take_while_subtype m ++ [⟨m,le_refl m⟩]),
+    { transitivity' (f ∘ coe).length_on (l.take_while_subtype m ++ [⟨m,le_refl m⟩]),
       { rw [←f.length_on_map coe, list.map_append, list.take_while_subtype_map_coe],
-        simp only [list.map], simp only [subtype.coe_mk],
-        finish, },
+        simp only [list.map, subtype.coe_mk], exact le_refl _, },
       { refine @le_supr₂ _ _ _ _ (λ l H, length_on (f ∘ coe) l) _ _,
-        rw list.pairwise_append,
-        dsimp [list.take_while_subtype], simp, sorry,  }
-
-    },
-    sorry,
-    -- Need to change the lists `left,right` to live in the correct sets
-  },
+        simp only [list.pairwise_append, mem_set_of_eq, list.mem_singleton],
+        refine ⟨list.take_while_subtype_pairwise_le _ _, list.pairwise_singleton _ _, _⟩,
+        rintro y yl _ rfl,
+        exact list.take_while_subtype_le_base m l y yl, }, },
+    { transitivity' (f ∘ coe).length_on
+        ([(⟨m,le_refl m⟩ : {x // m ≤ x})] ++ (l.drop_while_subtype_ge m ls)),
+      { rw [←f.length_on_map coe, list.map_append, list.drop_while_subtype_ge_map_coe],
+        simp only [list.map, subtype.coe_mk], exact le_refl _, },
+      { refine @le_supr₂ _ _ _ _ (λ l H, length_on (f ∘ coe) l) _ _,
+        simp only [list.singleton_append, mem_set_of_eq, list.pairwise_cons,
+                   subtype.mk_le_mk],
+        refine ⟨list.drop_while_subtype_ge_ge_base _ _ _,
+                list.drop_while_subtype_ge_pairwise_le _ _ _⟩, }, }, },
   { apply path_length_ge_parts,
-    rintro x xm y my, exact xm.trans my, },
+    rintro x xm y my, apply le_trans, exact xm, exact my, }, -- `xm.trans my` doesn't work
 end
 
 /--
@@ -384,57 +313,6 @@ A path is rectifiable if any of its restriction to a close interval has finite l
 def is_rectifiable :=
   ∀ (a b : β), (f ∘ (λ x : Icc a b, x.val)).path_length ≠ ⊤
 
-def arc_length (x₀ : ℝ) : ℝ → ℝ :=
-λ x,
-  if h : x₀ ≤ x then
-    (f ∘ inclusion (set.inter_subset_left s (Icc x₀ x))).path_length.to_real
-  else
-    - (f ∘ inclusion (set.inter_subset_left s (Icc x x₀))).path_length.to_real
-
-lemma arc_length_monotone (hs : f.is_rectifiable)
-  (x₀ : ℝ) (x₀s : x₀ ∈ s) : monotone (f.arc_length x₀) :=
-begin
-  rintro x₁ x₂ h,
-  dsimp [arc_length],
-  split_ifs with h₁ h₂ h₂ h₁,
-  { transitivity'
-      (path_length (f ∘ inclusion (set.inter_subset_left s (Icc x₀ x₁)))).to_real +
-      (path_length (f ∘ inclusion (set.inter_subset_left s (Icc x₁ x₂)))).to_real,
-    simp only [le_add_iff_nonneg_right, ennreal.to_real_nonneg],
-    rw ←ennreal.to_real_add, rotate, apply hs, apply hs,
-    refine ennreal.to_real_mono _ _, apply hs,
-    apply path_length_ge_parts, -- Should use specialized version of `path_length_ge_parts`
-    { rintro x ⟨xs,⟨x₀x,xx₁⟩⟩, exact ⟨xs,x₀x,xx₁.trans h⟩ },
-    { rintro x ⟨xs,⟨x₁x,xx₂⟩⟩, exact ⟨xs,h₁.trans x₁x,xx₂⟩},
-    { rintro x ⟨xs,x₀x,xx₁⟩ y ⟨ys,x₁y,yx₂⟩, exact xx₁.trans x₁y, }, },
-  { exfalso, apply h₂ (h₁.trans h), },
-  { transitivity' 0;
-    simp only [right.neg_nonpos_iff, ennreal.to_real_nonneg];
-    apply ennreal.to_real_nonneg, },
-  -- this whole block ↓ is taken from the first. Intelligent tactic management should help ged rid of it
-  { simp only [neg_le_neg_iff, not_le] at h₁ h₂ ⊢,
-    transitivity'
-      (path_length (f ∘ inclusion (set.inter_subset_left s (Icc x₁ x₂)))).to_real +
-      (path_length (f ∘ inclusion (set.inter_subset_left s (Icc x₂ x₀)))).to_real,
-    simp only [le_add_iff_nonneg_left, ennreal.to_real_nonneg],
-    rw ←ennreal.to_real_add, rotate, apply hs, apply hs,
-    refine ennreal.to_real_mono _ _, apply hs,
-    apply path_length_ge_parts,
-    { rintro x ⟨xs,⟨x₁x,xx₂⟩⟩, exact ⟨xs,x₁x,xx₂.trans h₂.le⟩ },
-    { rintro x ⟨xs,⟨x₂x,xx₀⟩⟩, exact ⟨xs,h.trans x₂x, xx₀⟩},
-    { rintro x ⟨xs,x₀x,xx₁⟩ y ⟨ys,x₁y,yx₂⟩, exact xx₁.trans x₁y, }, }
-end
-
--- TODO: constant speed reparameterization
-
 end path_length
-
-section curve_length
-
-variables {α : Type*} [metric_space α]
-
--- Say a curve has domain an Icc
-
-end curve_length
 
 end function
---
+++ src/topology/metric_space/path_preliminaries.lean
@@ -0,0 +1,97 @@
+import data.set.basic
+import data.list.basic
+import data.list.pairwise
+
+open set
+
+variables {α β : Type*} (x : α)
+
+lemma subtype.map_id_comp_id (α : Type*) (r s t : α → Prop)
+  (rs : ∀ x, r x → s x) (st : ∀ x, s x → t x) :
+  (subtype.map id st) ∘ (subtype.map id rs) = (subtype.map id $ λ x, (st x) ∘ (rs x)) :=
+funext (λ _, rfl)
+
+lemma subtype.coe_comp_map_id (α : Type*) (r s : α → Prop)
+  (rs : ∀ x, r x → s x) :
+  coe ∘ (subtype.map id rs) = (coe : (subtype r) → α) :=
+funext (λ _, rfl)
+
+def list.take_while_subtype [preorder α] [decidable_pred (≤x)] (l : list α) : list (subtype (≤x)) :=
+(l.take_while (≤x)).attach.map $ subtype.map id $ λ y, list.mem_take_while_imp
+
+lemma list.take_while_subtype_map_coe [preorder α] [decidable_pred (≤x)] (l : list α) :
+  (l.take_while_subtype x).map (coe : subtype (≤x) → α) = l.take_while (≤x) :=
+begin
+  dsimp only [list.take_while_subtype],
+  simp only [list.map_map],
+  apply list.attach_map_coe,
+end
+
+lemma list.pairwise_le_drop_while_le_not_le  [preorder α] [decidable_pred (≤x)] :
+  ∀ (l : list α) (h : l.pairwise (≤)) (y : α), y ∈ l.drop_while (≤x) → ¬y ≤ x
+| [] h y hy := by { simp only [list.drop_while, list.not_mem_nil] at hy, exact hy.elim }
+| (a::l) h y hy := by
+  { dsimp only [list.drop_while] at hy,
+    simp only [list.pairwise_cons] at h,
+    split_ifs at hy with ax nax,
+    { exact list.pairwise_le_drop_while_le_not_le l h.right y hy, },
+    { cases hy,
+      { cases hy, exact ax},
+      { exact λ yx, ax ((h.left y hy).trans yx), }, }, }
+
+def list.drop_while_subtype [preorder α] [decidable_pred (≤x)] (l : list α) (h : l.pairwise (≤)) :
+  list (subtype (λ y, ¬ y≤x)) :=
+(l.drop_while (≤x)).attach.map $ subtype.map id (λ y, l.pairwise_le_drop_while_le_not_le x h y)
+
+def list.drop_while_subtype_ge [linear_order α]  (l : list α) (h : l.pairwise (≤)) :
+  list (subtype (λ y, x≤y)) :=
+(l.drop_while_subtype x h).map $ subtype.map id $ λ y h', @le_of_not_le α _ y x h'
+
+lemma list.drop_while_subtype_ge_map_coe [linear_order α] (l : list α) (h : l.pairwise (≤)) :
+  (l.drop_while_subtype_ge x h).map coe = l.drop_while (≤x) :=
+begin
+  dsimp only [list.drop_while_subtype_ge, list.drop_while_subtype],
+  simp only [list.map_map, subtype.map_id_comp_id, subtype.coe_comp_map_id, list.attach_map_coe],
+end
+
+lemma list.take_while_subtype_pairwise_le [preorder α] [decidable_pred (≤x)] (l : list α) :
+  (l.take_while_subtype x).pairwise (≤) := sorry
+
+
+lemma list.take_while_subtype_le_base [preorder α] [decidable_pred (≤x)] (l : list α) :
+  ∀ y ∈ l.take_while_subtype x, ↑y ≤ x := sorry
+
+lemma list.drop_while_subtype_ge_pairwise_le [linear_order α] (l : list α) (h : l.pairwise (≤)) :
+  (l.drop_while_subtype_ge x h).pairwise (≤) := sorry
+
+lemma list.drop_while_subtype_ge_ge_base  [linear_order α] (l : list α) (h : l.pairwise (≤)) :
+  ∀ y ∈ l.drop_while_subtype_ge x h, x ≤ ↑y := sorry
+
+
+section
+
+open function set
+
+lemma right_inverse_monotone [linear_order α] [partial_order β]
+  (φ : α → β) (mφ : monotone φ) (ψ : β → α) (φψ : right_inverse ψ φ) : monotone ψ :=
+begin
+  rintro x y l,
+  cases le_total (ψ x) (ψ y),
+  { exact h },
+  { let := mφ h,
+    rw [φψ x, φψ y] at this,
+  cases le_antisymm l this, refl, },
+end
+
+lemma right_inverse_antitone [linear_order α] [partial_order β]
+  (φ : α → β) (mφ : antitone φ) (ψ : β → α) (φψ : right_inverse ψ φ) : antitone ψ :=
+begin
+  rintro x y l,
+  cases le_total (ψ y) (ψ x),
+  { exact h, },
+  { let := mφ h,
+    rw [φψ x, φψ y] at this,
+    cases le_antisymm l this, refl, },
+end
+
+end
