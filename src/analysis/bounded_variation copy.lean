1fec7e49b367149ef4c092b5617798aa53150e09
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2023-01-03 18:47:43 +0100

    wip

--- src/analysis/bounded_variation copy.lean
+++ src/analysis/bounded_variation copy.lean
@@ -38,9 +38,14 @@ end
 lemma _root_.edist_congr_right {a b c : E} (hab : edist a b = 0) : edist c a = edist c b :=
 by { rw [edist_comm c a, edist_comm c b], apply edist_congr_left hab,  }
 
+-- Thanks Floris van Doorn
 lemma _root_.ennreal.mul_div_mul_left {a b c : ℝ≥0∞} (ha : a ≠ 0) (ha' : a ≠ ⊤) :
-  a * b / (a * c) = b / c := sorry
-
+  a * b / (a * c) = b / c :=
+begin
+  simp_rw [div_eq_mul_inv],
+  rw [ennreal.mul_inv (or.inl ha) (or.inl ha'), mul_mul_mul_comm, ennreal.mul_inv_cancel ha ha',
+    one_mul],
+end
 
 
 
@@ -53,14 +58,6 @@ begin
   rw edist_congr_left (h $ p.snd.prop.2 (i+1)),
   rw edist_congr_right (h $ p.snd.prop.2 i),
 end
-
-
-/-
- theorem metric.tendsto_uniformly_on_iff {α : Type u} {β : Type v} [pseudo_metric_space α]
- {ι : Type u_1} {F : ι → β → α} {f : β → α} {p : filter ι} {s : set β} :
-tendsto_uniformly_on F f p s ↔ ∀ (ε : ℝ), ε > 0 → (∀ᶠ (n : ι) in p, ∀ (x : β), x ∈ s → has_dist.dist (f x) (F n x) < ε)
-
--/
 
 lemma eps_approx {f : α → E} {s : set α} (hs: s.nonempty) (h : has_bounded_variation_on f s)
   (ε : ℝ≥0∞) (hε : ε > 0) :
@@ -75,19 +72,43 @@ begin
   exact supr_le (λ h, (hn h).le),
 end
 
+lemma eps_approx' {f : α → E} {s : set α} (hs: s.nonempty) (v : ℝ≥0∞) (h : v < evariation_on f s) :
+  ∃ (p : ℕ × {u : ℕ → α // monotone u ∧ ∀ i, u i ∈ s}),
+    v ≤ ∑ i in finset.range p.1, edist (f ((p.2 : ℕ → α) (i+1))) (f ((p.2 : ℕ → α) i))  :=
+begin
+  sorry,
+end
+
 lemma evariation_on_lower_continuous {ι : Type*} {F : ι → α → E} {p : filter ι}
-  {f : α → E} {s : set α} (hF : tendsto_uniformly_on F f p s) (hf : has_bounded_variation_on f s) :
-  ∀ (ε : ℝ≥0), ε ≠ 0 → (∀ᶠ (n : ι) in p, evariation_on f s ≤ evariation_on (F n) s + ε) :=
+  {f : α → E} {s : set α} (hF : tendsto_uniformly_on F f p s) :
+  ∀ (v : ℝ≥0∞), v < evariation_on f s → (∀ᶠ (n : ι) in p, v ≤ evariation_on (F n) s) :=
 begin
   by_cases hs : s.nonempty, swap,
-  { simp only [evariation_on.subsingleton f (λ x hx _ _, (hs ⟨x,hx⟩).elim),
-               zero_le', filter.eventually_true, implies_true_iff], },
-  rintro ε hε,
+  { simp only [evariation_on.subsingleton f (λ x hx _ _, (hs ⟨x,hx⟩).elim)],
+    simp only [implies_true_iff, not_lt_zero, is_empty.forall_iff], },
+  rintro v hv,
   rw emetric.tendsto_uniformly_on_iff at hF,
-  obtain ⟨⟨n,⟨u,um,us⟩⟩,hlt⟩ := eps_approx hs hf ((ε : ℝ≥0∞)/2) (half_pos (coe_ne_zero.mpr hε)),
-  specialize hF ((ε : ℝ≥0∞)/(4*n))
-    (ennreal.div_pos_iff.mpr ⟨coe_ne_zero.mpr hε, mul_ne_top (coe_ne_top) (coe_ne_top)⟩),
-  refine (hF.mono $ λ i hi, hlt.trans (le_trans _ (add_le_add_right (sum_le (F i) n um us) ε))),
+  obtain ⟨v',vv',v'var⟩ := exists_between hv,
+  obtain ⟨⟨n,⟨u,um,us⟩⟩,hlt⟩ := eps_approx' hs v' v'var,
+  let ε := (v' - v)/(2*n),
+  have hε : ε > 0 := sorry,
+  have hε' : ε ≠ ⊤ := sorry,
+  specialize hF ε hε,
+  apply hF.mono, clear hF,
+  rintro i hi,
+  suffices h : v' ≤ evariation_on (F i) s + (v' - v),
+  { simp at h, },
+
+
+  calc v'
+     ≤ ∑ (j : ℕ) in finset.range n, edist (f (u j.succ)) (f (u j))  : sorry
+  ...≤ ∑ (j : ℕ) in finset.range n, (edist (F i (u j.succ)) (F i (u j)) + 2*ε) : sorry
+  ...= ∑ (j : ℕ) in finset.range n, edist (F i (u j.succ)) (F i (u j)) + 2*n*ε : sorry
+  ...= ∑ (j : ℕ) in finset.range n, edist (F i (u j.succ)) (F i (u j)) + (v' - v) : sorry
+  ...≤ evariation_on (F i) s  + (v' - v) : sorry --sum_le (F i) _ _ _
+
+  /-apply hlt.trans,
+  -- (le_trans _ (add_le_add_right (sum_le (F i) n um us) _))),
   clear hlt hF,
   have : (ε : ℝ≥0∞) = (↑ε/2) + (↑ε/2), simp only [ennreal.add_halves],
   nth_rewrite_rhs 0 this, rw ←add_assoc,
@@ -95,11 +116,10 @@ begin
   calc ∑ (j : ℕ) in finset.range n, edist (f (u j.succ)) (f (u j))
      ≤ ∑ (j : ℕ) in finset.range n, (edist (F i (u j.succ)) (F i (u j)) + ↑ε/(2*n)) : by
   begin
-    refine finset.sum_le_sum (λ j jn, _),
-    apply (edist_triangle4 _ (F i (u j.succ)) (F i (u j)) _).trans,
+    refine finset.sum_le_sum (λ j jn, (edist_triangle4 _ (F i (u j.succ)) (F i (u j)) _).trans _),
     have : (ε : ℝ≥0∞)/(2*↑n) = ε/(4*n) + ε/(4*n), by
     { rw [ennreal.div_add_div_same, ←two_mul,
-          (by {norm_cast, rw ←mul_assoc, refl, } : (4 : ennreal) * ↑n = 2 * (2 * ↑n)),
+          (by { norm_cast, rw ←mul_assoc, refl, } : (4 : ennreal) * ↑n = 2 * (2 * ↑n)),
           ennreal.mul_div_mul_left (ennreal.two_ne_zero) (ennreal.two_ne_top)], },
     rw [this, ←add_assoc],
     refine add_le_add _ _,
@@ -114,7 +134,13 @@ begin
     rw [ennreal.le_div_iff_mul_le (or.inl ennreal.two_ne_zero) (or.inl ennreal.two_ne_top),
         mul_comm, ←mul_assoc, mul_comm ((2 : ℝ≥0∞) * ↑n)],
     apply ennreal.mul_le_of_le_div (le_refl _),
-  end
+  end-/
+end
+
+lemma evariation_on_lower_continuous' {f : α → E} {s : set α} (hf : has_bounded_variation_on f s) :
+  lower_semicontinuous_at (λ g, evariation_on g s) f :=
+begin
+  rintro v hv,
 end
 
 end evariation_on
