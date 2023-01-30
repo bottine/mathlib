04469c2cb459f5ced341d40d574e7d66dd04d617
ART0 <18333981+0Art0@users.noreply.github.com>
2023-01-30 12:24:24 +0530

    partial progress, starting refactoring soon

--- src/combinatorics/simple_graph/ends/functoriality.lean
+++ src/combinatorics/simple_graph/ends/functoriality.lean
@@ -9,6 +9,8 @@ universes u v w
 constant simple_graph.edist {V : Type*} (G : simple_graph V) (u v : V) : ℕ∞
 constant simple_graph.dist_triangle {V : Type*} (G : simple_graph V) (u v w : V) :
   G.edist u v ≤ G.edist u w + G.edist w v
+constant simple_graph.reachable_iff_edist_lt_top {V : Type*} (G : simple_graph V) (u v : V) :
+  G.reachable u v ↔ G.edist u v < ⊤
 
 variables {V V' : Type u} (G : simple_graph V) (G' : simple_graph V')
 
@@ -20,6 +22,8 @@ def coarse_equal_with (K : ℕ∞) (f g : V → V'):=
   ∀ x : V, G'.edist (f x) (g x) < K
 
 section lipschitz
+
+variables {G} {G'}
 
 -- can be derived from `lipschitz_hom`
 theorem lipschitz_id : coarse_lipschitz_with G G 2 1 id := by {
@@ -36,7 +40,7 @@ theorem lipschitz_hom (φ : G →g G') : coarse_lipschitz_with G G' 2 1 φ := by
   sorry, -- another easy goal
 }
 
-theorem lipschitz_up (f : V → V') {K K' : ℕ∞} {C C' : ℕ} (hK : K ≤ K') (hC : C ≤ C')
+theorem lipschitz_up {f : V → V'} {K K' : ℕ∞} {C C' : ℕ} (hK : K ≤ K') (hC : C ≤ C')
   (hf : coarse_lipschitz_with G G' K C f)
   : coarse_lipschitz_with G G' K' C' f := by {
     rw [coarse_lipschitz_with],
@@ -50,11 +54,41 @@ theorem lipschitz_infty_wlog {P : (V → V') → Sort*} (C : ℕ) :
   ∀ (f : V → V') (K : ℕ∞) (hf : coarse_lipschitz_with G G' K C f), P f ↔
   ∀ (f : V → V') (hf : coarse_lipschitz_with G G' ⊤ C f), P f := sorry
 
-theorem lipschitz_infty_iff (f : V → V') (C : ℕ) (hf : coarse_lipschitz_with G G' ⊤ C f) :
-  ∀ x y : V, G.reachable x y → G'.reachable (f x) (f y) := sorry
+theorem lipschitz_infty_iff (f : V → V') {C : ℕ} :
+  (coarse_lipschitz_with G G' ⊤ C f) ↔ (∀ x y : V, G.reachable x y → G'.reachable (f x) (f y)) := by {
+    split,
+    {
+      intros hf x y hreach,
+      rw [simple_graph.reachable_iff_edist_lt_top],
+      have edist_bound := hf x y,
+      have : ∀ (K : ℕ∞) (n : ℕ), ⊤ * K + n = ⊤ := by {
+        intros K n,
+        sorry -- needs missing `enat` lemmas
+        },
+      rw [this] at edist_bound,
+      exact edist_bound,
+    },
+    {
+      intros hreach x y,
+      simp_rw [simple_graph.reachable_iff_edist_lt_top] at hreach,
+      by_cases h:G.edist x y < ⊤,
+      { sorry, },
+      { have : G.edist x y = ⊤ := sorry,
+        rw this,
 
-def lipschitz_comp_map (f : V → V') (K : ℕ∞) (C : ℕ) (hf : coarse_lipschitz_with G G' K C f) :
-  G.connected_component → G'.connected_component := sorry
+      }
+
+    }
+  }
+
+def lipschitz_comp_map {f : V → V'} {K : ℕ∞} {C : ℕ} (hf : coarse_lipschitz_with G G' K C f) :
+  G.connected_component → G'.connected_component :=
+    simple_graph.connected_component.lift (λ v, G'.connected_component_mk (f v)) (by {
+      intros _ _ p _,
+      rw simple_graph.connected_component.eq,
+      apply (lipschitz_infty_iff f).mp,
+      apply lipschitz_up le_top (nat.le_refl C) hf,
+      exact nonempty.intro p, })
 
 end lipschitz
 
@@ -69,7 +103,22 @@ structure coarse_map {V V' : Type u} (G : simple_graph V) (G' : simple_graph V')
     coarse_lipschitz_with (G.out $ finset_mapping L) (G'.out L)
       κ C (induce_out φ (finset_inv_sub L)))
 
-def coarse_map.end_map {f : V → V'} (fcoarse : coarse_map G G' f) : G.end → G'.end := sorry
+variables {G} {G'}
+
+def coarse_map.end_map {f : V → V'} (fcoarse : coarse_map G G' f) : G.end → G'.end := by
+  {
+    rintro ⟨Gsec, Gend⟩,
+    refine ⟨λ L, _, _⟩,
+    let comp_map := lipschitz_comp_map (fcoarse.induced_coarse_lipschitz L.unop),
+    apply comp_map,
+    let Gcomp := Gsec (opposite.op $ fcoarse.finset_mapping (opposite.unop L)),
+    exact Gcomp,
+    {
+      intros L L' hLL',
+      simp,
+      sorry,
+    },
+  }
 
 def coarse_equal.end_equal (f g : V → V') (K : ℕ∞) (fcoarse : coarse_map G G' f) (gcoarse : coarse_map G G' g)
   (close : coarse_equal_with G' K f g) : coarse_map.end_map G G' fcoarse = coarse_map.end_map G G' gcoarse := sorry
